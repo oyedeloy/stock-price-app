63896e2a4dc461158018d95ce6110a512ce30069
dele <oyedeloy@gmail.com>
2025-03-22 16:49:16 -0400

    ECR workflow added to Github actions

---
+++ terraform/ecr.tf
@@ -0,0 +1,14 @@
+module "ecr_backend" {
+  source    = "./modules/ecr"
+  repo_name = "stock-backend"
+}
+
+module "ecr_frontend" {
+  source    = "./modules/ecr"
+  repo_name = "stock-frontend"
+}
+
+module "ecr_redis" {
+  source    = "./modules/ecr"
+  repo_name = "stock-redis"
+}
---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,15 @@
+resource "aws_ecr_repository" "this" {
+  name = var.repo_name
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name = var.repo_name
+  }
+}
